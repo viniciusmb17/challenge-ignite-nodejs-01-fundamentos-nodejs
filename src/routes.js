968ccf9ae83af7567a7c274cd2d79ecb5798f3ad
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-06-27 17:00:00 -0300

    feat: List and Create Task

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,40 @@
 import { Database } from './database.js'
 import { buildRoutePath } from './utils/build-route-path.js'
+import { randomUUID } from 'node:crypto'
 
 const database = new Database()
 
+/**
+ * Task:
+  - `id` - Identificador único de cada task
+  - `title` - Título da task
+  - `description` - Descrição detalhada da task
+  - `completed_at` - Data de quando a task foi concluída. O valor inicial deve ser `null`
+  - `created_at` - Data de quando a task foi criada.
+  - `updated_at` - Deve ser sempre alterado para a data de quando a task foi atualizada.
+ */
+
 export const routes = [
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: null,
+      }
+
+      database.insert('tasks', task)
+
+      return res.writeHead(201).end()
+    },
+  },
   {
     method: 'GET',
     path: buildRoutePath('/tasks'),
